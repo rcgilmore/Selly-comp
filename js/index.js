76a0fa28c38fb44a279cc7e8a5e2de286e18cf24
Rebecca Gilmore <Rebecca Gilmore>
2016-06-21 18:25:48 +0700

    arrow buttons work, sidebar sticks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,38 @@
+
 $(document).ready(function(){
-  $(".down-arrow")on.('click', function() {
-  location.href = $(this).attr.next;
+  $(".down-arrow").on('click', function() {
+    var nextSectionId = $(this).attr("next");
+    var nextSection = $('#' + nextSectionId);
+    $(window).scrollTo(nextSection, {duration:800});
+    return false;
+  });
+
+var waypoint = new Waypoint({
+  element: $('.content')[0],
+  handler: function(direction) {
+    console.log(direction);
+    $('.sidebar').addClass('menu-fixed');
+  }
+});
+
+var waypoint = new Waypoint({
+  element: $('.stripes')[0],
+  handler: function(direction) {
+    console.log(direction);
+    $('.sidebar').removeClass('menu-fixed');
+    }
   });
 });
+
+// var waypoint = new Waypoint({
+//   element: $('attr-section-id')[0],
+//   handler: function() {
+//     console.log();
+//
+//   });
+// });
+// Update each menu item on the right to have an element parameter attr-section-id that has a value of the id of the section you want to target.
+
+// create new waypoint
+// element should be content
+// handler function should trigger sidebar becoming fixed
